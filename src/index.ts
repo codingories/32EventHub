6e70e1f3b871800b5646b63b50b1ac26b5ea0ac6
朱骏杰 <ories.zhu@mochain.vip>
2020-08-05 11:11:33 +0800

    补充注释

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ class EventHub {
   //  '羊城晚报' : [fn1, fn2, fn3]
   // }
   on(eventName, fn){
+    // 把 fn 推进 this.cache[eventName] 数组
     // eventName订报纸,fn报纸放到邮箱
     // eventName = 楚天, fn
     if(this.cache[eventName] === undefined){
@@ -14,6 +15,7 @@ class EventHub {
     array.push(fn);
   }
   emit(eventName){
+    // 把 this.cache[eventName] 数组里面的 fn 全部依次调用
     let array = this.cache[eventName];
     if(array=== undefined){
       array = []
