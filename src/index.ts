a1e89ebc8afdd819d913708dec1ce53935a930f0
朱骏杰 <ories.zhu@mochain.vip>
2020-08-05 11:05:29 +0800

    写完on

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 class EventHub {
+  cache = {};
+  // {
+  //  '楚天都市报': [fn1, fn2, fn3],
+  //  '羊城晚报' : [fn1, fn2, fn3]
+  // }
   on(eventName, fn){
-  // eventName订报纸,fn报纸放到邮箱
+    // eventName订报纸,fn报纸放到邮箱
+    // eventName = 楚天, fn
+    if(this.cache[eventName] === undefined){
+      this.cache[eventName] = []
+    }
+    const array = this.cache[eventName];
+    array.push(fn);
   }
   emit(eventName){}
 }
